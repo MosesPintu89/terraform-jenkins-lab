42ddf5231c53f5658f72add9ec04a60b940ac500
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2026-09-06 11:47:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 resource "aws_instance" "jenkins_ec2" {
   ami           = "ami-0b6c6ebed2801a5cb"
   instance_type = "t3.micro"
-
+  subnet_id = "subnet-0d5d7fc07ce20c89e"
   tags = {
     Name = "Jenkins-Terraform-Instance"
   }
